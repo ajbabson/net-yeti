289787959b8442bdb417ba40e58d1bf61fe8c842
Aric Babson <aricb@protonmail.com>
2021-12-05 10:34:24 +0100

    updated with test ranges and comments

--- tools/modules/ipdb.py
+++ tools/modules/ipdb.py
@@ -1,8 +1,10 @@
 
 import re
 
+# add the destination directory to save the zone files
 zone_dir = 'zones/'
 
+# add your custom SOA to add to the top of each zone file
 soa = """$TTL 4h
 @ IN SOA ns1.example.com. admin.example.com. (
      1     ; Serial
@@ -15,11 +17,20 @@ soa = """$TTL 4h
 ns1            IN A      192.0.2.0
 """
 
-# regex to match the networks you want to match
-re_apnic1_v4  = re.compile(r'^(103\.214\.228\.\d+)')
-re_ripe1_v4   = re.compile(r'^(185\.37\.22[0-3]\.\d+)')
-re_ripe2_v4   = re.compile(r'^(77\.111\.2(0[89]|1[01])\.\d+)')
-re_arin1_v4   = re.compile(r'^(192\.206\.95\.\d+)')
+# add one regex for each network you want to match
+#
+# these are fake public ranges for illustrative purposes 
+# imagine you have two RIPE allocations and one APNIC
+# RIPE  192.0.2.0/24
+# RIPE  198.51.100.0/24
+# APNIC 203.0.113.0/24
+# replace these with your own public ranges and the appropriate description
+# the description doesn't matter as long as it matches what you put in the
+# zones list below this section
+re_ripe1_v4   = re.compile(r'^(192\.0\.2\.\d+)')
+re_ripe2_v4   = re.compile(r'^(198\.51\.100\.\d+)')
+re_apnic1_v4  = re.compile(r'^(203\.0\.113\.\d+)')
+# RFC 1918
 re_10_v4      = re.compile(r'^(10\.\d+\.\d+\.\d+)')
 re_172_16_v4  = re.compile(r'^(172\.16\.\d+\.\d+)')
 re_172_17_v4  = re.compile(r'^(172\.17\.\d+\.\d+)')
@@ -39,11 +50,15 @@ re_172_30_v4  = re.compile(r'^(172\.30\.\d+\.\d+)')
 re_172_31_v4  = re.compile(r'^(172\.31\.\d+\.\d+)')
 re_192_168_v4 = re.compile(r'^(192\.168\.\d+\.\d+)')
 
+# Each tuple should be the name of the zone file you want to write
+# and the regex that matches the IP range that will go in the zone file.
+# The regex variable must be declared in the section above.
 zones = [
-    ('db.185.37', re_ripe1_v4),
-    ('db.77.111', re_ripe2_v4),
-    ('db.192.206.95', re_arin1_v4),
-    ('db.103.214.228', re_apnic1_v4),
+    # public
+    ('db.192.0.2', re_ripe1_v4),
+    ('db.198.51.100', re_ripe2_v4),
+    ('db.203.0.113', re_apnic1_v4),
+    # RFC 1918
     ('db.10', re_10_v4),
     ('db.172.16', re_172_16_v4),
     ('db.172.17', re_172_17_v4),
@@ -64,13 +79,20 @@ zones = [
     ('db.192.168', re_192_168_v4)
 ]
 
+# Add to this dict any records that are not contained in your network config
+# backups.
+# The key must match the name of a zone file declared in the zones list above.
+# The value should be the exact text as you want it to appear at the top of the
+# zone file just below the SOA.  This is an optional section and can be removed
+# as long as the static records = {} declaration remains in place as an empty
+# dict.
 static_records = {
     'db.10': """
 ; servers - manually added
-2.1.0          IN PTR    abc-bastion-01.example.com.
-6.1.0          IN PTR    abc-bastion-02.example.com.
-14.1.0         IN PTR    xyz-bastion-01.example.com.
-18.1.0         IN PTR    xyz-bastion-02.example.com.
+2.1.0          IN PTR    abc-server-01.example.com.
+6.1.0          IN PTR    abc-server-02.example.com.
+14.1.0         IN PTR    xyz-server-01.example.com.
+18.1.0         IN PTR    xyz-server-02.example.com.
 """,
     'db.172.16': """
 ; links - manually added
